9067711c5181a4e6a0a446d66087bc6ee4b98626
EthicalSoftwareDevelopment <EthicalSoftwareDevelopment>
2024-09-11 11:48:34 +0200

    Mechanical Oscillations

---
+++ C#/TheOpenMathLibrary.Engineering/MechanicalOscillations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheOpenMathLibrary.Engineering
+{
+    /// <summary>
+    /// Mechanical energy oscillation functions
+    /// </summary>
+    public class MechanicalOscillations
+    {
+        /// <summary>
+        /// SHM Energy function 
+        /// </summary>
+        /// <param name="mass"></param>
+        /// <param name="amplitude"></param>
+        /// <param name="angularFrequency"></param>
+        /// <returns></returns>
+        public static double SHMEnergy(double mass, double amplitude, double angularFrequency)
+        {
+            double energy = 0;
+            energy = 0.5 * mass * amplitude * amplitude * angularFrequency * angularFrequency;
+            return energy;
+        }
+
+        /// <summary>
+        /// DHM Energy function 
+        /// </summary>
+        /// <param name="mass"></param>
+        /// <param name="amplitude"></param>
+        /// <param name="angularFrequency"></param>
+        /// <returns></returns>
+        public static double DHMEnergy(double mass, double amplitude, double angularFrequency)
+        {
+            double energy = 0;
+            energy = 0.5 * mass * amplitude * amplitude * angularFrequency * angularFrequency;
+            return energy;
+        }
+    }
+}
